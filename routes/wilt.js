242c7cb1ebb8f70add2c8e66de0f2be1ebce4818
Prasheel Soni <prasheelsoni11@gmail.com>
2020-11-17 19:49:14 +0530

    feat(wilt): add get all wilts route

--- routes/wilt.js
+++ routes/wilt.js
@@ -48,4 +48,13 @@ router.post('/update/:id', async (req, res) => {
     }
 });
 
+router.get('/', async (req, res) => {
+    try {
+        const result = await Wilt.find({});
+        res.status(200).send(result);
+    } catch (e) {
+        res.status(404).send(e.message);
+    }
+});
+
 module.exports = router;
